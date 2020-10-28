c27d4e3ba9103e7e647fed9c22e5eae0328827dd
nishidate-yuki <asta18425@gmail.com>
2020-10-28 20:39:16 +0900

    add write_options

--- options.js
+++ options.js
@@ -1,13 +1,25 @@
 // chrome.storageに保存
 function save_options() {
-	var json_text = document.getElementById('json_text').value;
-	chrome.storage.sync.set(
-		{
-			json_text: json_text
-		},
-		function () { }
-	);
+    var json_text = document.getElementById('json_text').value;
+    chrome.storage.sync.set(
+        {
+            json_text: json_text
+        },
+        function () { }
+    );
 }
+
+function write_options() {
+    chrome.storage.sync.get(
+        ["json_text"],
+        function (items) {
+            // get json
+            // let jsonObject = JSON.parse(items.json_text);
+            document.getElementById('json_text').value = items.json_text;
+        }
+    )
+}
+
 
 // // chrome.storageに保存されている環境設定を使用して、セレクトボックスとチェックボックスの状態を復元します。
 // function restore_options() {
@@ -20,6 +32,6 @@ function save_options() {
 // 		document.getElementById('like').checked = items.likesColor;
 // 	});
 // }
-// document.addEventListener('DOMContentLoaded', restore_options);
 
+document.addEventListener('DOMContentLoaded', write_options);
 document.getElementById('save').addEventListener('click', save_options);
