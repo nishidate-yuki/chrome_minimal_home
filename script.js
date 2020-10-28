9e6ce1fc745668d41548c68ac59263db152262a6
nishidate-yuki <asta18425@gmail.com>
2020-10-28 20:59:42 +0900

    add default json

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ function add_links() {
     chrome.storage.sync.get(
         ["json_text"],
         function (items) {
+            if (items.json_text == undefined) {
+                items.json_text = "{\r\n    \"num_colmuns\" : 4,\r\n\r\n    \"title\"       : \"New tab\",\r\n\r\n    \"width\"       : 40,\r\n\r\n    \"img_size\"    : 50,\r\n\r\n    \"back_color\"  : \"#ffffff\",\r\n\r\n    \"links\": [\r\n        {\r\n            \"page_url\" : \"https:\/\/www.youtube.com\/\",\r\n            \"img_url\"  : \"https:\/\/simpleicons.org\/icons\/youtube.svg\"\r\n        },\r\n        {\r\n            \"page_url\" : \"https:\/\/twitter.com\/\",\r\n            \"img_url\"  : \"https:\/\/simpleicons.org\/icons\/twitter.svg\"\r\n        },\r\n        {\r\n            \"page_url\" : \"https:\/\/music.apple.com\/\",\r\n            \"img_url\"  : \"https:\/\/simpleicons.org\/icons\/applemusic.svg\"\r\n        },\r\n        {\r\n            \"page_url\" : \"https:\/\/www.behance.net\/\",\r\n            \"img_url\"  : \"https:\/\/simpleicons.org\/icons\/behance.svg\"\r\n        }\r\n    ]\r\n}";
+                chrome.storage.sync.set(
+                    {
+                        json_text: items.json_text
+                    },
+                    function () { }
+                );
+            }
             // get json
             let jsonObject = JSON.parse(items.json_text);
 
